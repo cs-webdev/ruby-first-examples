9733e2aefdc822f0a85a34d61ac666983c39c1aa
Zamith <zamith.28@gmail.com>
2014-06-07 17:06:20 +0100

    Adds kittens (each_kittens method missing)

--- cat.rb
+++ cat.rb
@@ -2,8 +2,24 @@ require_relative 'printable'
 
 class Cat
   include Printable
+  attr_reader :name
 
-  def name
-    "Garfield"
+  def initialize(name)
+    @kittens = []
+    @name = name
   end
+
+  def give_birth(number_of_kittens)
+    1.upto number_of_kittens do |num|
+      @kittens << Cat.new("Garfield no. #{num}")
+    end
+  end
+end
+
+garfield = Cat.new "Garfield"
+
+garfield.give_birth 5
+
+garfield.each_kitten do |kitten|
+  puts kitten.name
 end
